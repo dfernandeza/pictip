e62e64b44c9063fece4fef3edf31f30af47ea53c
Daniel Fernandez <danyfernandeza@gmail.com>
2014-07-30 23:16:24 -0600

    Adding Pictip demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+$('#pictip-demo').pictip({
+	spots: [
+		{top: '228px', left: '400px', content: '<p>Surfboard goes here!</p>', tooltipPosition:'tr'/*, tooltipClose: true*/},
+		{top: '330px', left: '185px', content: '<p>Just get in and drive!</p>', tooltipPosition:'tr'},
+		{top: '345px', left: '345px', content: '<p>VW Van, sweet!</p>', tooltipPosition:'bl'}
+	]
+});
